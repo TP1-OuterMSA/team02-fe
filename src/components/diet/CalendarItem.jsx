f50eaafc8510efca497fa95e0887547db015e75e
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-04-14 15:56:04 +0900

    [chore] calendaritem 추가

---
+++ src/components/diet/CalendarItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendarItem = ({day,date}) => {
+  return (
+      <div>
+        <p></p>
+        <div><p></p></div>
+      </div>
+  );
+};
+
+export default CalendarItem;
