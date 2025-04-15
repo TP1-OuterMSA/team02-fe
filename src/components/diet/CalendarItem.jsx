b76005196940ceea154f3d0d3ccc520de470ffce
choikyungsoo <ryan1954@naver.com>
2025-04-15 22:27:13 +0900

    [feat] 주별로 날짜 변경되도록 기능 추가

--- src/components/diet/CalendarItem.jsx
+++ src/components/diet/CalendarItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import clsx from 'clsx';
 
-const CalendarItem = ({day,date}) => {
+const CalendarItem = ({idx, date, isToday}) => {
+  const weekDays = ["월", "화", "수", "목", "금", "토", "일"];
   return (
-      <div>
-        <p></p>
-        <div><p></p></div>
+      <div className="flex flex-col items-center">
+        <p className={clsx("text-lg font-semibold mb-1.5", isToday ? "text-(--primary) font-bold" : "text-zinc-600")}>{weekDays[idx]}</p>
+        <div className={clsx("w-9 h-9 rounded-full flex items-center justify-center", isToday ? "bg-(--primary)" : "")}>
+          <p className={clsx("text-base font-semibold", isToday ? "text-white" : "text-zinc-600")}>{date}</p>
+        </div>
       </div>
   );
 };
--- src/pages/Diet.jsx
+++ src/pages/Diet.jsx
@@ -2,18 +2,54 @@ import {icRight, icLeft, imgMainCharcter} from "@assets/index.js";
 import LineButton from "@components/common/LineButton.jsx";
 import {string} from "@utils/string.js";
 import Graph from "@components/diet/Graph.jsx";
+import CalendarItem from "@components/diet/CalendarItem.jsx";
+import dayjs from "dayjs";
+import 'dayjs/locale/ko';
+dayjs.locale("ko");
+
+import {useState} from "react";
 
 const Diet = () => {
+
+  const today = dayjs();
+  const [startOfWeek, setStartOfWeek] = useState(today.startOf("week").add(1, "day"));
+
+  const generateWeek = () => {
+    return Array.from({ length: 7 }).map((_, idx) => startOfWeek.add(idx, "day"));
+  };
+
+  const handlePrevWeek = () => {
+    setStartOfWeek(startOfWeek.subtract(7, "day"));
+  };
+
+  const handleNextWeek = () => {
+    setStartOfWeek(startOfWeek.add(7, "day"));
+  };
+
+  const handleToday = () => {
+    setStartOfWeek(today.startOf("week").add(1, "day"));
+  };
+
+  const week = generateWeek();
+  console.log(week);
   return (
     <div className="pl-7 pr-7 flex gap-5">
       <div className="w-full">
         <div className="flex justify-between items-center w-full">
-          <p className="text-black text-2xl font-extrabold">2025.04.15 ~ 05.15</p>
+          <p className="text-black text-2xl font-extrabold">
+            {startOfWeek.format("YYYY.MM.DD")} ~ {startOfWeek.add(6, "day").format("MM.DD")}
+          </p>
           <div className="flex gap-2">
-            <LineButton><img src={icLeft} alt="icon" className="w-6 h-6"/></LineButton>
-            <LineButton><img src={icRight} alt="icon" className="w-6 h-6"/></LineButton>
-            <LineButton><span className="text-black text-xl font-bold ml-1 mr-1">오늘</span></LineButton>
+            <LineButton onClick={handlePrevWeek}><img src={icLeft} alt="icon" className="w-6 h-6"/></LineButton>
+            <LineButton onClick={handleNextWeek}><img src={icRight} alt="icon" className="w-6 h-6"/></LineButton>
+            <LineButton onClick={handleToday}><span className="text-black text-xl font-bold ml-1 mr-1">오늘</span></LineButton>
           </div>
+        </div>
+        <div className="flex justify-between pl-2 pr-2 mt-5">
+          {week.map((date, idx) => {
+            const isToday = date.isSame(today, "day");
+            return <CalendarItem key={idx} idx={idx} date={date.format("DD")} isToday={isToday} />
+          })}
         </div>
       </div>
       <div className="w-100">
