3f085f861a67566add1b4c187acd7bfc76ef3840
choikyungsoo <ryan1954@naver.com>
2025-04-24 16:56:12 +0900

    [fix] 드롭다운 호버 효과 수정 및 작성자에 따른 메뉴 노출 수정

--- src/components/common/DropDown.jsx
+++ src/components/common/DropDown.jsx
@@ -1,11 +1,11 @@
 import {string} from "@utils/string.js";
 
-const DropDown = ({onClick}) => {
+const DropDown = ({onClick, isShow}) => {
   return (
     <div className="w-40 max-w-72 min-w-40 bg-white rounded-lg shadow-[0px_2px_6px_2px_rgba(0,0,0,0.15)] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30)] p-2">
-      <p className="dropdown_menu" onClick={() => onClick(string.EDIT)}>{string.EDIT}</p>
-      <p className="dropdown_menu" onClick={() => onClick(string.EDIT)}>{string.BLOCK}</p>
-      <p className="dropdown_menu text-red-600" onClick={() => onClick(string.DELETE)}>{string.DELETE}</p>
+      {isShow && <p className="dropdown_menu" onClick={() => onClick(string.EDIT)}>{string.EDIT}</p>}
+      {!isShow && <p className="dropdown_menu" onClick={() => onClick(string.BLOCK)}>{string.BLOCK}</p>}
+      {isShow && <p className="dropdown_menu text-red-600 hover:bg-red-100/50" onClick={() => onClick(string.DELETE)}>{string.DELETE}</p>}
     </div>
   );
 };
