09ef35e1dcb9072ba0bf92fb8bf327c7e3f4a64c
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-04-30 22:59:22 +0900

    [feat] 먹는 gif 이미지 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -52,6 +52,7 @@ export {default as imgMainCharcter} from '@assets/images/img_main_character.svg'
 export {default as imgWelcome} from '@assets/images/img_welcome.svg';
 export {default as imgNoodle} from '@assets/images/img_noodle.svg';
 export {default as imgEatRice} from '@assets/images/img_eat_rice.gif';
+export {default as imgEating} from '@assets/images/img_eating.gif';
 
 
 
