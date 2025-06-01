e8a64e3e869675840c82a8ce9241a7dc4c1891cf
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-06-01 10:32:44 +0900

    마지막 데모용 입니다.  (#29)
    * [feat] 달력표시 UI 제작
    
    * [design] 식단분석 전체 틀 완성
    
    * [design] 디자인 전체 컴포넌트 제작
    
    * [feat] 주간, 일일 api 연결
    
    * [fix] RecommendFood 컴포넌트 사이즈 값 수정
    
    * [feat] swiper.js 적용 및 추천메뉴 빈화면 처리
    
    * [feat] 칼로리 계산 및 day별 가져오는 api 코드 추가
    
    * [feat] 추천메뉴 더미데이터 추가
    
    * [feat] 선형 그래프 추가
    
    * [feat] 식사메이트 ui 완성 및 저장 api 완료
    
    * [feat[ 글 작성 및 조회 api 연결
    
    * [feat] 매칭 목록 fab 추가
    
    * [feat] 매칭중 오류 수정  및 기타 api 연결 완료
    
    * [feat] 행사정보 연결 및 데모 완료
    
    * [feat[ SSE 연동
    
    * [feat] matchpost 작업중 중간 저장
    
    * [feat] 매칭 수정 작업중...
    
    * [feat] 식사메이트 수정 api 연동
    
    * [feat] 매칭 요청 삭제 api연동
    
    * [feat] 매칭목록 불러오기 api 연동 완료
    
    * [feat] 매칭 요청에 대한 매칭의사 표현 api 연동
    
    * [feat] 매칭 수락/거절 api 연동
    
    * feat] sse연동 버전
    
    * [chore] user 연결에 따른 api 구조 변경
    
    * [feat]이미지 클릭시 team6으로 이동하도록 설정
    
    * [fix] 추가된 식사 없이 저장하고 닫기 클릭시 닫히도록 수정
    
    * [feat] 작업완료
    
    * [fix] userId 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { useRoutes } from "react-router-dom";
 import RoutesConfig from "@/routes/routesConfig.jsx";
+import userService from "@apis/user/userService.js";
 import {ToastContainer} from "react-toastify";
 import {useCustomNavigation} from "@hooks/useCustomNavigation.js";
 import {useCustomWindowNavigation} from "@hooks/useCustomWindowNavigation.js";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {pagePath} from "@/routes/pagePath.js";
 
 function App() {
@@ -13,15 +14,21 @@ function App() {
   const {navigateToWindow} = useCustomWindowNavigation();
 
 
+  const patchUserId = async () => {
+    const data =  await userService.getMyUserId();
+    localStorage.setItem('userId', data);
+  }
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
     if(accessToken){
       navigateTo(pagePath.ROOT)
+      patchUserId();
     } else{
       navigateTo(pagePath.LOGIN)
       // navigateToWindow("/team6/login")
     }
   }, []);
+  
   return (
     <div>
       {router}
