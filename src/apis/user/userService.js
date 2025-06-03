75e2b3f1f8dd68564db77c1f9a54928f22023a64
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-06-03 16:16:49 +0900

    [Fix/demo] 경로 설정 오류에 따른 재 PR (#35)
    * [refactor] 유저정보 변경 사항 적용
    
    * [fix] react버전 18버전으로 다운그레이드
    
    * [fix] msa맞는 라우팅 설정
    
    * [fix] msa용 라우팅
    
    * fix: routing route
    
     - fix routing route
    
    * [fix] msa 용 유저 정보 경로 설정
    
    * [fix] 404에러에 대한 경로 수정
    
    ---------
    
    Co-authored-by: user122938 <69978045+user122938@users.noreply.github.com>

--- src/apis/user/userService.js
+++ src/apis/user/userService.js
@@ -1,6 +1,6 @@
 import {axiosInstance} from "@apis/axiosInstance.js";
 
-const GT_PREFIX = import.meta.env.VITE_ABSOLUTE_URL + "/api/team06-api-gateway";
+const GT_PREFIX = import.meta.env.VITE_ABSOLUTE_URL + "/api/team06-api-gateway/api/team2";
 
 export const getRecommendKcal = async () => {
   const response = await axiosInstance.get(`${GT_PREFIX}/user/getRecommendKcal`);
